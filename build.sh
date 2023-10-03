e33924690e15b929b9f86c9fcbf6ada3b3ce67ed
mauro-verhoest <mauro.verhoest@hotmail.com>
2023-10-03 17:44:28 +0200

    remove loop from ./run.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+#Sset -x
 #https://www.baeldung.com/linux/shebang
-set -x
+
 cd ~/
 #https://docs.docker.com/engine/reference/commandline/build/
 docker build --platform=linux/arm/v8  -f docker-matter-hub/Dockerfile -t matter-hub . 
+echo "etwade"
+exit 0
+echo "dit kan niet"
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 #https://www.baeldung.com/linux/shebang
 set -x
 #sudo service otbr-agent stop
-./build.sh 
-./run.sh
+#https://www.baeldung.com/linux/start-script-after-another-finishes
+./start.sh &
+wait && ./build.sh
+exit 0
